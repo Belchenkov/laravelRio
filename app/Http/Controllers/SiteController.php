b61e2c00c42009984451e10ad0a3d8b12c17fa83
Aleksey Belchenkov <u608110@gmail.com>
2019-02-08 07:21:02 +0300

    Output Main Navigation in Template

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -27,7 +27,7 @@ class SiteController extends Controller
     {
         $menu = $this->getMenu();
 
-        $navigation = view(env('THEME') . '.navigation')->render();
+        $navigation = view(env('THEME') . '.navigation')->with('menu', $menu)->render();
         $this->vars = array_add($this->vars, 'navigation', $navigation);
 
         return view($this->template)->with($this->vars);
@@ -37,6 +37,18 @@ class SiteController extends Controller
     {
         $menu = $this->m_rep->get();
 
-        return $menu;
+        $mBuilder = \Menu::make('MyNav', function ($m) use ($menu) {
+            foreach ($menu as $item) {
+                if ($item->parent == 0) {
+                    $m->add($item->title, $item->path)->id($item->id);
+                } else {
+                    if ($m->find($item->parent)) {
+                        $m->find($item->parent)->add($item->title, $item->path)->id($item->id);
+                    }
+                }
+            }
+        });
+
+        return $mBuilder;
     }
 }
