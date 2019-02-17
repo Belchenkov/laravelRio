de0adf96e46fbf1a253a9b84f679f6722609f9d4
Aleksey Belchenkov <u608110@gmail.com>
2019-02-17 07:37:53 +0300

    Create Relations of Article, output articles

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -24,16 +24,22 @@ class ArticlesController extends SiteController
     public function index()
     {
         $articles = $this->getArticles();
-        dd($articles);
+        $content = view(env('THEME') . '.articles_content')->with('articles', $articles)->render();
+        $this->vars = array_add($this->vars, 'content', $content);
+
         return $this->renderOutput();
     }
 
     public function getArticles($alias = false)
     {
-        $articles = $this->a_rep->get(['title', 'alias', 'created_at', 'img', 'desc'], false, true);
+        $articles = $this->a_rep->get(
+            ['id', 'title', 'alias', 'created_at', 'img', 'desc', 'user_id', 'category_id'],
+            false,
+            true
+        );
 
         if ($articles) {
-            //$articles->load('user', 'category', 'comments');
+            $articles->load('user', 'category', 'comments');
         }
 
         return $articles;
